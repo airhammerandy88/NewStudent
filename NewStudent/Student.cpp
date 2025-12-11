b603ba947d76bcd333446ae3b83cf0f7055b9c56
airhammerandy <andrew0a0jones@gmail.com>
2025-12-10 22:54:20 -0500

    new version master

---
+++ NewStudent/Student.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include "Student.h"
+
+using namespace std;
+
+
+
+
+
+Student::Student()
+{
+}
+
+Student::Student(string studentId, string firstName, string lastName, string emailAddress, int age, DegreeProgram degreeProgram)// need completed
+{
+}
+
+string Student::GetStudentId() const
+{
+    return string();
+}
+
+string Student::GetFirstName() const
+{
+    return firstName;
+}
+
+string Student::GetLastName() const
+{
+    return lastName;
+}
+
+string Student::GetEmailAddress() const
+{
+    return emailAddress;
+}
+
+int Student::GetAge() const
+{
+    return age;
+}
+
+void Student::SetStudentID(string sId)
+{
+    studentId = sId;
+}
+
+void Student::SetFirstName(string fName)
+{
+    firstName = fName;
+
+}
+
+void Student::SetLastName(string lName)
+{
+    lastName = lastName;
+}
+
+void Student::SetEmailAddres(string eAddress)
+{
+    emailAddress = eAddress;
+}
+
+void Student::SetAge(int aGe)
+{
+    age = aGe;
+}
+
+void Student::PrintStudentData(string firstName, string lastName, string emailAddress, DegreeProgram degreeProgram) //degree
+{
+    cout << "A1   " << "First Name:   " << firstName << "    " << endl;  //strange tab spacing
+    cout << "Last name:   " << lastName << "   " << endl;
+    cout << "Email:   " << emailAddress << "   " << endl;
+    cout << "Age:   " << age << "    " << endl;
+    cout << "daysInCourse:   " << GetNumDaysToCompleteCourse << "   " << endl;
+    cout << "Degree Program:    " << degreeProgram << "   " << endl;
+}
---
+++ NewStudent/roster.cpp
@@ -0,0 +1,88 @@
+#include "roster.h"
+
+Student* classRosterArray[5];
+int nextIndex = 0;
+
+
+
+Roster::Roster()
+{
+	classRosterArray[5] = new Student[5];
+}
+
+Roster::~Roster()
+{
+	delete[] classRosterArray[5];
+}
+
+void Roster::parse(string studentData) //pulls from main studentdata array
+{
+
+	size_t rhs = studentData.find(",");
+	string firstName = studentData.substr(0, rhs);
+
+	size_t lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	string lastName = studentData.substr(lhs, rhs - lhs);
+
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	string emailAddress = studentData.substr(lhs, rhs - lhs);
+
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int age = stoi(studentData.substr(lhs, rhs - lhs));
+
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int numDays1 = stoi(studentData.substr(lhs, rhs - lhs));
+
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int numDays2 = stoi(studentData.substr(lhs, rhs - lhs));
+
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	int numDays3 = stoi(studentData.substr(lhs, rhs - lhs));
+
+	lhs = rhs + 1;
+	rhs = studentData.find(",", lhs);
+	enum degree = (studentData.substr(lhs, rhs - lhs)); //need to find solution for degree 
+
+
+
+
+
+
+	add(firstName, lastName, emailAddress, age, numDays1, numDays2, numDays3);
+}
+
+void Roster::add(string studentId, string lastName, string emailAddress, int age, int daysInCourse1, int daysIncourse2, int daysInCourse3, DegreeProgram degreeProgram) //add degree
+{
+	int* daysInCourse = new int[3] {daysInCourse1, daysIncourse2, daysInCourse3};
+	classRosterArray.push_back(new Student(studentId, firstname, lastName, emailAddress, age, daysInCourse, degreeProgram);
+}
+
+void Roster::remove(string studentId)
+{
+
+}
+
+void Roster::printAll()
+{
+	for (int i = 0; i < 5 ++i) {
+		classRosterArray[i].print();//change to relivent data
+	}
+}
+
+void Roster::printAverageDaysInCourse(string studentId)
+{
+}
+
+void Roster::printInvaildEmails()
+{
+}
+
+void Roster::printBydegreeProgram(DegreeProgram degreProgram)
+{
+}
